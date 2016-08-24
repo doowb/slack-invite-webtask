ee926ac1a2c0bf876c51bc35bd7176b5a62c6645
doowb <brian.woodward@gmail.com>
2016-08-24 17:44:45 -0400

    implementation

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 'use strict';
 
-module.exports = function() {
+var invite = require('slack-invite');
+
+module.exports = function (ctx, cb) {
+  var email = ctx.data.email;
+  var team = ctx.data.slack_team;
+  var token = ctx.data.slack_token;
+
+  if (!team) {
+    return cb(new Error('Invalid slack team'));
+  }
+
+  if (!token) {
+    return cb(new Error('Invalid slack token'));
+  }
+
+  if (!email) {
+    return cb(new Error('Invalid email address'));
+  }
+
+  var options = {
+    email: email,
+    team: team,
+    token: token,
+    channels: 'general'
+  };
+
+  invite(options, function(response) {
+    cb();
+  });
 };
