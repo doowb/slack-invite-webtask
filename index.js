b5885ec38abd0ebb5686ac7ad6deac27c14ef67d
doowb <brian.woodward@gmail.com>
2016-08-24 18:51:27 -0400

    refactor to handle req and res directly to enforce on POST methods fix bugs on which data properties are used

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var invite = require('slack-invite');
+var url = require('url');
 
-module.exports = function (ctx, cb) {
+module.exports = function (ctx, req, res) {
+  var cb = done(res);
+
+  if (req.method !== 'POST') {
+    return cb(new Error('Invalid HTTP METHOD'));
+  }
+
   var email = ctx.data.email;
-  var team = ctx.data.slack_team;
-  var token = ctx.data.slack_token;
+  var team = ctx.data.SLACK_TEAM;
+  var token = ctx.data.SLACK_TOKEN;
 
   if (!team) {
-    return cb(new Error('Invalid slack team'));
+    return cb(new Error('Invalid SLACK_TEAM property.'));
   }
 
   if (!token) {
-    return cb(new Error('Invalid slack token'));
+    return cb(new Error('Invalid SLACK_TOKEN property.'));
   }
 
   if (!email) {
@@ -27,6 +34,22 @@ module.exports = function (ctx, cb) {
   };
 
   invite(options, function(response) {
-    cb();
+    if (response && response.body) {
+      cb(null, response.body);
+      return;
+    }
+    cb(null, {success: true});
   });
 };
+
+function done(res) {
+  return function(err, data) {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({error: err.message}));
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(typeof data === 'object' ? JSON.stringify(data) : data);
+  };
+}
